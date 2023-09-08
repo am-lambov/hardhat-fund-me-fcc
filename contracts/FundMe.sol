e6f4de434fdba7a7f2d98ab4e00640602e2844aa
Lambov <lambov@protonmail.com>
2023-09-08 23:08:08 +0300

    update to follow solidity styling best practices

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -4,57 +4,70 @@ pragma solidity ^0.8.8;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "./PriceConverter.sol";
 
-error NotOwner();
+error FundMe__NotOwner();
 
+/// @title A contract for crowd funding
+/// @author Lambov
+/// @notice This contract was implemented following Patrick Collins' FreeCodeCamp course https://www.youtube.com/watch?v=gyMwXuJrbJQ
+/// @dev This implements price feeds as our library
 contract FundMe {
     using PriceConverter for uint256;
 
     mapping(address => uint256) public addressToAmountFunded;
+
+    address public immutable i_owner;
     address[] public funders;
 
-    address public /* immutable */ i_owner;
     uint256 public constant MINIMUM_USD = 1 * 10 ** 18;
 
     AggregatorV3Interface public priceFeed;
-    
+
+    modifier onlyOwner() {
+        if (msg.sender != i_owner) revert FundMe__NotOwner();
+        _;
+    }
+
     constructor(address priceFeedAddress) {
         i_owner = msg.sender;
         priceFeed = AggregatorV3Interface(priceFeedAddress);
-    }
-
-    function fund() public payable {
-        require(msg.value.getConversionRate(priceFeed) >= MINIMUM_USD, "You need to spend more ETH!");
-        addressToAmountFunded[msg.sender] += msg.value;
-        funders.push(msg.sender);
-    }
-    
-    function getVersion() public view returns (uint256){
-        return priceFeed.version();
-    }
-    
-    modifier onlyOwner {
-        // require(msg.sender == owner);
-        if (msg.sender != i_owner) revert NotOwner();
-        _;
-    }
-    
-    function withdraw() public onlyOwner {
-        for (uint256 funderIndex=0; funderIndex < funders.length; funderIndex++){
-            address funder = funders[funderIndex];
-            addressToAmountFunded[funder] = 0;
-        }
-        funders = new address[](0);
-
-        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
-        require(callSuccess, "Call failed");
-    }
-
-    fallback() external payable {
-        fund();
     }
 
     receive() external payable {
         fund();
     }
 
+    fallback() external payable {
+        fund();
+    }
+
+    /// @notice This function funds this contract
+    function fund() public payable {
+        require(
+            msg.value.getConversionRate(priceFeed) >= MINIMUM_USD,
+            "You need to spend more ETH!"
+        );
+        addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
+    }
+
+    function getVersion() public view returns (uint256) {
+        return priceFeed.version();
+    }
+
+    function withdraw() public onlyOwner {
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(callSuccess, "Call failed");
+    }
 }
